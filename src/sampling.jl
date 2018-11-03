d1d98e670210f86a160700a8e2704c4dcb2b2c12
anj1 <alireza.n.j@gmail.com>
2018-11-04 10:34:36 +1300

    Add dims to cat

--- src/sampling.jl
+++ src/sampling.jl
@@ -6,7 +6,7 @@
 function rand_concentrations(n_species, total_conc)
     # see 'Uniform sampling from a simplex' from StackExchange
     # Generate a list of sorted numbers from 0 to 1
-    lst = cat(1, [0.0], sort(rand(n_species-1)), [1.0])
+    lst = cat(dims=1, [0.0], sort(rand(n_species-1)), [1.0])
     # Take differences between consecutive elements
     return total_conc*(lst[2:end]-lst[1:end-1])
 end 
